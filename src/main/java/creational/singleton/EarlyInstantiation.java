1f048ab539ccf79522e43f76f635f48f736862ff
iuliia_gorshkova <gorshkovayulia93@gmail.com>
2022-01-08 19:07:59 +0300

    Add 'Singleton' pattern: early and lazy initialization

---
+++ src/main/java/creational/singleton/EarlyInstantiation.java
@@ -0,0 +1,19 @@
+package creational.singleton;
+
+/*
+* In such case, we create the instance of the class at the time of declaring the static data member,
+* so instance of the class is created at the time of classloading
+ */
+public class EarlyInstantiation {
+
+    // Gets memory only once because of static
+    private static EarlyInstantiation instance = new EarlyInstantiation();
+
+    // Prevent to instantiate the Singleton class from outside the class
+    private EarlyInstantiation() {}
+
+    // Provides the global point of access to the Singleton object and returns the instance to the caller
+    public static EarlyInstantiation getInstance() {
+        return instance;
+    }
+}
---
+++ src/main/java/creational/singleton/LazyInstantiation.java
@@ -0,0 +1,28 @@
+package creational.singleton;
+
+/*
+* In such case, we create the instance of the class in synchronized method or synchronized block,
+* so instance of the class is created when required
+ */
+public class LazyInstantiation {
+
+    // Object is "volatile" since we want to ensure that multiple threads offer the object variable correctly
+    // when it is being initialized to Singleton instance.
+    // This method drastically reduces the overhead of calling the synchronized method every time.
+    private static volatile LazyInstantiation instance;
+
+    // Prevent to instantiate the Singleton class from outside the class
+    private LazyInstantiation() {}
+
+    // Provides the global point of access to the Singleton object and returns the instance to the caller
+    public static LazyInstantiation getInstance() {
+        if (instance == null) {
+            synchronized (LazyInstantiation.class) {
+                if (instance == null) { // check again as multiple threads can reach above step
+                    instance = new LazyInstantiation();
+                }
+            }
+        }
+        return instance;
+    }
+}
