179622a1d64650a44d0fbf8f48a9af604e32745b
iuliia_gorshkova <gorshkovayulia93@gmail.com>
2022-03-31 10:25:51 +0300

    Fix comment

--- src/main/java/structural/adapter/MovableAdapter.java
+++ src/main/java/structural/adapter/MovableAdapter.java
@@ -1,7 +1,9 @@
 package structural.adapter;
 
-// MovableAdapter interface is based on the same Movable class.
-// It may be slightly modified to yield different results in different scenarios.
+/**
+ * MovableAdapter interface is based on the same Movable class.
+ * It may be slightly modified to yield different results in different scenarios.
+ */
 public interface MovableAdapter {
     // returns speed in KM/H
     double getSpeed();
