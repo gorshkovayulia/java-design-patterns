d952224fdd770beccb2b2e4eb997d47c7a904981
Iuliia Gorshkova <gorshkovayulia93@gmail.com>
2022-05-11 21:14:20 +0300

    Add Factory Method pattern

---
+++ src/main/java/creational/factorymethod/JpegReader.java
@@ -0,0 +1,15 @@
+package creational.factorymethod;
+
+public class JpegReader implements ImageReader {
+
+    private DecodedImage decodedImage;
+
+    public JpegReader(String image) {
+        decodedImage = new DecodedImage(image);
+    }
+
+    @Override
+    public DecodedImage getDecodeImage() {
+        return decodedImage;
+    }
+}
